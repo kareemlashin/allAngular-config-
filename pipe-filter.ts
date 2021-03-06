3849a0e212971f5a2bf84ce29b47a4511e0700db
kareemlashin <58936937+kareemlashin@users.noreply.github.com>
2021-03-06 03:28:59 +0200

    Update pipe-filter.ts

--- pipe-filter.ts
+++ pipe-filter.ts
@@ -8,3 +8,8 @@ transform(Restaurants: any[], search: string): any {
       return x.Name.toLowerCase().includes(search.toLowerCase());
     });
   }
+https://stackoverflow.com/questions/34164413/how-to-apply-filters-to-ngfor
+https://stackoverflow.com/questions/46780843/angular-4-filter-search-custom-pipe
+https://stackoverflow.com/questions/50761509/how-to-filter-the-list-using-pipes-in-angular-2
+https://stackoverflow.com/questions/40152034/angular-2-typescript-pipes-filter-not-working
+https://stackoverflow.com/questions/54128768/angular-6-filter-observable-array-with-pipe
